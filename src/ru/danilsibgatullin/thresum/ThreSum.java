f77437edc0924d14ef1cb8818dd98bc43990debd
DanilSibgatullin <danilsibgatullin@gmail.com>
2020-12-06 22:08:43 +0300

    Compleate ThreeSum

--- src/ru/danilsibgatullin/thresum/ThreSum.java
+++ src/ru/danilsibgatullin/thresum/ThreSum.java
@@ -1,13 +1,35 @@
 package ru.danilsibgatullin.thresum;
 
+
+import java.util.HashSet;
+
+
 public class ThreSum {
 
     public static void main(String[] args){
 
-        int[] num ={-1,0,1,2,-1,-4};
-        for (int i=0;i<sumElemetsIs0(num).length;i++)
-        System.out.print(sumElemetsIs0(num)[i]+" ");
+        int[] num ={-1,0,0}; // Вводный массив
 
+        System.out.println("Обрабатываемый массив");
+        for (int i=0;i< num.length;i++){
+            System.out.print(num[i] + " ");
+        }
+        System.out.println();
+        if (num.length<3){
+            System.out.println("Введенный массив  не содержит 3-х элементов для проверки условия a + b + c = 0");
+            return;
+        }
+        System.out.println("Элементы массива удовлетворяющие условию a + b + c = 0");
+        HashSet<Integer[]> outSet = new HashSet<>();
+        if (outSet.isEmpty()){
+             System.out.println("Указанному условию не соответсвует ни одна из комбинаций");
+        }
+        for (Integer[] arr : outSet){
+            for (int i=0;i<arr.length;i++){
+                System.out.print(arr[i]+ " ");
+            }
+            System.out.println();
+        }
     }
 
     /*
@@ -16,24 +38,22 @@ public class ThreSum {
     Input: nums = [-1,0,1,2,-1,-4]
     Output: [[-1,-1,2],[-1,0,1]]
      */
-    static int[] sumElemetsIs0 (int[] arr){
-        boolean isEmpty = true;
-        int[] outPutArray = new int[3];
+    static HashSet<Integer[]> sumElemetsIs0 (int[] arr){
+        HashSet<Integer[]> outSet = new HashSet<>(); //для выводва используем HashSet так как нас интересуют только уникальные значения
         for (int a=0;a<arr.length;a++){
+            Integer[] outPutArray = new Integer[3];
             for (int b=a+1;b<arr.length;b++){
                 for (int c=b+1;c<arr.length;c++){
                     if (arr[a]+arr[b]+arr[c]==0){
                         outPutArray[0] = arr[a];
                         outPutArray[1] = arr[b];
                         outPutArray[2] = arr[c];
-                        isEmpty = false;
-                        return outPutArray;
+                        outSet.add(outPutArray);
                     }
                 }
             }
         }
-
-        return outPutArray;
+        return outSet;
     }
 
 }
